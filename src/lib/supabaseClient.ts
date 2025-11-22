df4d63e0b81ed1b7de4bae724d850ec403b40030
Esau <92233843+Esau-Devs@users.noreply.github.com>
2025-11-21 21:22:31 -0600

    fix url

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 // src/lib/supabaseClient.ts
 
+// src/lib/supabaseClient.ts
+
 import { createClient } from '@supabase/supabase-js'
-import type { SupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl: string = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey: string = import.meta.env.PUBLIC_SUPABASE_ANON;
@@ -14,4 +15,12 @@ if (!supabaseUrl || !supabaseAnonKey) {
     );
 }
 
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,
+        storageKey: 'sb-auth-token',
+        storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+    }
+});
